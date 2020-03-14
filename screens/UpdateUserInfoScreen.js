2608316eae6f6b29d2a7f8fb22fdd8e0f57e3136
Xaylus <cocodoom2018@gmail.com>
2020-03-14 23:12:57 +0000

    Update UpdateUserInfoScreen.js
    added password changing capabilities

--- screens/UpdateUserInfoScreen.js
+++ screens/UpdateUserInfoScreen.js
@@ -4,6 +4,7 @@ import {
   View,
   Button,
   TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 class UpdateUserInfo extends Component{
@@ -14,6 +15,10 @@ constructor(props){
     given_name: "",
     family_name: "",
     email: "",
+    changePassword: false,
+    old_password: "",
+    new_password: "",
+    confirm_password:"",
   }
 
   this.Submit = this.Submit.bind(this);
@@ -31,30 +36,75 @@ constructor(props){
   Submit(){
     console.log( this.state.given_name + " " + this.state.family_name + " " +
                     this.state.email);
-    return fetch("http://10.0.2.2:3333/api/v0.0.5/user/" + global.user_id,{
-      method:'PATCH',
-      headers: {
-        'X-Authorization': global.token,
-      },
-      body: JSON.stringify({
-        given_name: this.state.given_name,
-        family_name: this.state.family_name,
-        email: this.state.email,
-      })
-    }).then((response) =>{
-      if(response.status == '201'){
-        alert("complete");
-        this.props.navigation.navigate('UserProfile');
-      } else{
-        alert("error occurred");
-      }
-    }).catch((error)=>{
-      console.log(error);
-    });
+
+if(this.state.changePassword){
+      return fetch("http://10.0.2.2:3333/api/v0.0.5/user/" + global.user_id,{
+        method:'PATCH',
+        headers: {
+          'X-Authorization': global.token,
+        },
+        body: JSON.stringify({
+          given_name: this.state.given_name,
+          family_name: this.state.family_name,
+          email: this.state.email,
+          password: this.state.new_password,
+        })
+      }).then((response) =>{
+        if(response.status == '201'){
+          alert("complete");
+          this.props.navigation.navigate('UserProfile');
+        } else{
+          alert("error occurred");
+        }
+      }).catch((error)=>{
+        console.log(error);
+      });
+    } else {
+      return fetch("http://10.0.2.2:3333/api/v0.0.5/user/" + global.user_id,{
+        method:'PATCH',
+        headers: {
+          'X-Authorization': global.token,
+        },
+        body: JSON.stringify({
+          given_name: this.state.given_name,
+          family_name: this.state.family_name,
+          email: this.state.email,
+        })
+      }).then((response) =>{
+        if(response.status == '201'){
+          alert("complete");
+          this.props.navigation.navigate('UserProfile');
+        } else{
+          alert("error occurred");
+        }
+      }).catch((error)=>{
+        console.log(error);
+      });
+    }
   }
 
   Cancel(){
     this.props.navigation.navigate('UserProfile');
+  }
+
+  addPassword(){
+    if(this.state.changePassword){
+      return(
+        <View>
+          <TextInput placeholder="Old Password"
+          onChangeText={(text) => this.setState({old_password:text})}
+          value={this.state.old_password} />
+
+          <TextInput placeholder="New Password"
+          onChangeText={(text) => this.setState({new_password:text})}
+          value={this.state.new_password} />
+
+          <TextInput placeholder="Confirm password"
+          onChangeText={(text) => this.setState({confirm_password: text})}
+          value={this.state.confirm_password} />
+        </View>
+      );
+    }
   }
 
   render(){
@@ -67,6 +117,11 @@ constructor(props){
         value={this.state.family_name}/>
         <TextInput onChangeText={(text) => this.setState({email:text})}
         value={this.state.email} />
+
+        <TouchableOpacity onPress={() => this.setState({changePassword: true})} >
+          <Text> Change Password? </Text>
+        </TouchableOpacity>
+        {this.addPassword()}
 
         <Button title="Submit"
         onPress={this.Submit} />
