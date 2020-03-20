9cd1193c67dfd30474c7e3b6d4e15b3a6ffd1753
Xaylus <cocodoom2018@gmail.com>
2020-03-20 02:33:28 +0000

    Update UpdateUserInfoScreen.js
    Added functionality to change images for the profile picture

--- screens/UpdateUserInfoScreen.js
+++ screens/UpdateUserInfoScreen.js
@@ -5,7 +5,9 @@ import {
   Button,
   TextInput,
   TouchableOpacity,
+  Image,
 } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
 class UpdateUserInfo extends Component{
 constructor(props){
@@ -19,10 +21,46 @@ constructor(props){
     old_password: "",
     new_password: "",
     confirm_password:"",
+    gotImage: false,
+    changedImage: false,
+    Image:null
   }
-
   this.Submit = this.Submit.bind(this);
   this.Cancel = this.Cancel.bind(this);
+  this.imagePicker = this.imagePicker.bind(this);
+  this.renderImage = this.renderImage.bind(this);
+  this.postProfilePicture = this.postProfilePicture.bind(this);
+}
+
+imagePicker(){
+  const options = {
+    title: 'Select Avatar',
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+  ImagePicker.showImagePicker(options, (response) => {
+    console.log('Response = ', response);
+
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    } else if (response.error) {
+      console.log('ImagePicker Error: ', response.error);
+    } else if (response.customButton) {
+      console.log('User tapped custom button: ', response.customButton);
+    } else {
+  //const source = { uri: response.uri };
+    console.log(response);
+  // You can also display the image using data:
+  // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+      this.setState({
+        Image: response,
+        changedImage: true,
+      });
+    }
+  });
 }
 
   componentDidMount(){
@@ -31,6 +69,12 @@ constructor(props){
       family_name: this.state.userData.family_name,
       email: this.state.userData.email,
     });
+    if(this.props.navigation.state.params.Image !== undefined){
+      this.setState({
+        Image: this.props.navigation.state.params.Image,
+        gotImage: true,
+      });
+    }
   }
 
   Submit(){
@@ -41,6 +85,7 @@ constructor(props){
       return fetch("http://10.0.2.2:3333/api/v0.0.5/user/" + global.user_id,{
         method:'PATCH',
         headers: {
+          'Content-Type' : 'application/json',
           'X-Authorization': global.token,
         },
         body: JSON.stringify({
@@ -51,7 +96,10 @@ constructor(props){
         })
       }).then((response) =>{
         if(response.status == '201'){
-          alert("complete");
+        //  alert("complete");
+          if(this.state.changedImage){
+            this.postProfilePicture();
+          }
           this.props.navigation.navigate('UserProfile');
         } else{
           alert("error occurred");
@@ -72,7 +120,10 @@ constructor(props){
         })
       }).then((response) =>{
         if(response.status == '201'){
-          alert("complete");
+          //alert("complete");
+          if(this.state.changedImage){
+            this.postProfilePicture();
+          }
           this.props.navigation.navigate('UserProfile');
         } else{
           alert("error occurred");
@@ -81,6 +132,25 @@ constructor(props){
         console.log(error);
       });
     }
+  }
+
+  postProfilePicture(){
+    return fetch("http://10.0.2.2:3333/api/v0.0.5/user/photo",{
+    method: 'POST',
+    headers: {
+      'X-Authorization': global.token
+    },
+    body: this.state.Image
+  }).then((response) =>{
+    if(response.status == 201){
+      alert("completed");
+      //this.props.navigation.navigate('Home');
+    } else {
+      alert("Something went wrong");
+    }
+  }).catch((error) => {
+    console.log(error)
+    });
   }
 
   Cancel(){
@@ -107,10 +177,24 @@ constructor(props){
     }
   }
 
+renderImage(){
+  if(this.state.gotImage){
+    return(
+      <View>
+        <Image style={{width: 50, height: 50}}
+          source={{uri : this.state.Image.uri}} />
+      </View>
+    )
+  }
+
+}
   render(){
     return(
       <View>
         <Text>My Profile</Text>
+        {this.renderImage()}
+        <Button title="Change profile picture"
+        onPress={() => this.imagePicker()} />
         <TextInput onChangeText={(text) => this.setState({given_name: text})}
         value={this.state.given_name} />
         <TextInput onChangeText={(text) => this.setState({family_name: text})}
